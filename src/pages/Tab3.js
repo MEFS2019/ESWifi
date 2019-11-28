db7c381d43aa05bf75afd504d51099a88cc8b1e0
Santi Espada <santi.espada@gmail.com>
2019-11-28 12:30:48 +0100

    feat: initial bootstrap

---
+++ src/pages/Tab3.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  IonHeader,
+  IonToolbar,
+  IonPage,
+  IonTitle,
+  IonContent
+} from "@ionic/react";
+
+const Tab3Page = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab Three</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent />
+    </IonPage>
+  );
+};
+
+export default Tab3Page;
