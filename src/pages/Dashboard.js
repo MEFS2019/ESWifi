632f40c9d03d9a8fc5ca472115daa1ef24872fcc
fernando.sanchiz.delainfiesta <fernando.sanchiz.delainfiesta.next@bbva.com>
2019-11-29 14:49:49 +0100

    Add dashboard without working buttons

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import {
+    IonHeader,
+    IonToolbar,
+    IonPage,
+    IonTitle,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonModal,
+    IonButton
+} from "@ionic/react";
+
+import BtnDashboard from "components/BtnDashboard";
+
+const listFeatures = [
+    { data: "Wifi Pass", state: false, route: "wifiPass"},
+    { data: "SSID", state: true, route: "ssid"},
+    { data: "Admin Pass", state: true, route: "adminPass"},
+    { data: "Cypher", state: false, route: "cypher"},
+    { data: "MACs Filter", state: true, route: "mac"},
+    { data: "Allow IPs", state: true, route: "ips"},
+    { data: "Signal Power", state: true, route: "signal"}
+]
+
+const Dashboard = props => {
+
+    const [showModal, setShowModal] = useState(false);
+
+    //TODO No funciona el método
+    const handleRoute = (event, route) => {
+        event.preventDefault();
+        const { history } = props;
+        console.log(route);
+        setShowModal(true);
+        history.push("/dashboard/" + route);
+    };
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                <IonTitle>Dashboard</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent >
+                <IonGrid>
+                    {listFeatures.map(({ data, state, route }, i) => (
+                    <IonRow key={i}>
+                        <BtnDashboard onClick={event =>  { handleRoute(event, route) }} data={data} state={state}></BtnDashboard>
+                    </IonRow>
+                    ))}
+                </IonGrid>
+                <IonModal isOpen={showModal}>
+                    <p>This is modal content</p>
+                    <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
+                </IonModal>
+            </IonContent>
+    </IonPage>
+    );
+};
+
+export default Dashboard;
