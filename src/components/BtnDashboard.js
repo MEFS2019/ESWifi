f0a56733d7648049e8e876936ace3d59108fdd39
fernando.sanchiz.delainfiesta <fernando.sanchiz.delainfiesta.next@bbva.com>
2019-11-30 03:17:10 +0100

    Fix css

--- src/components/BtnDashboard.js
+++ src/components/BtnDashboard.js
@@ -14,7 +14,7 @@ const BtnDashboard = props => {
     const { data: name, state: color, onClick } = props;
 
     return (
-        < IonButton onClick={onClick} style={{width: '65%'}} shape="round" expand="block" size="large" name="test" >
+        < IonButton onClick={onClick} style={{width: '75%'}} shape="round" expand="block" size="large" name="test" >
             <IonItem lines="none" color="#3880ff" style={{width: '100%', font: "inherit"}}>
                 <IonIcon slot="start" color={ color ? "success" : "danger" } icon={checkbox}  />
                 {name}
